e0840e6ec7236616ee1a15606ba299b11e8bcdb3
Steven Jumbo <stevenalexanderjumbojaramillo@gmail.com>
2025-02-01 11:25:06 -0500

    Cambios en los modelos 'TareaTableModel' y en los Servicios 'TareaFileManagment'

--- src/Modelos/TareaTableModel.java
+++ src/Modelos/TareaTableModel.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelos;
 
-/**
- *
- * @author luisr
- */
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
 public class TareaTableModel {
-    
+    private final String[] columnNames = { "Nombre", "Nro. Pomodoros", "Tiempo de Enfoque", "Acciones" };
+    private final List<Tarea> tareas;
+
+    public TareaTableModel(List<Tarea> tareas) {
+        this.tareas = tareas;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public int getRowCount() {
+        return tareas.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Tarea tarea = tareas.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return tarea.getNombre();
+            case 1:
+                return tarea.getNumeroPomodoros();
+            case 2:
+                return tarea.getTiempoEnfoque();
+            case 3:
+                return "Eliminar";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return columnIndex == 3;
+    }
+
+    public void removeRow(int rowIndex) {
+        tareas.remove(rowIndex);
+        fireTableRowsDeleted(rowIndex, rowIndex);
+    }
+
+    public Tarea getTarea(int rowIndex) {
+        return tareas.get(rowIndex);
+    }
 }
--- src/Servicios/TareaFileManagment.java
+++ src/Servicios/TareaFileManagment.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servicios;
 
-/**
- *
- * @author luisr
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import Modelos.Tarea;
+
 public class TareaFileManagment {
-    
+    private static final String FILE_NAME = "tasks.txt";
+
+    public Integer getTaskId() {
+        return FileManagement.countLines(FILE_NAME) + 1;
+    }
+
+    public void createTask(Tarea tarea) {
+        FileManagement.createFile(FILE_NAME);
+        String content = tarea.getId() + "|" + tarea.getNombre() + "|" + tarea.getTiempoEnfoque() + "|"
+                + tarea.getNumeroPomodoros() + "|" + tarea.getUsername() + ";";
+        FileManagement.writeFile(FILE_NAME, content);
+    }
+
+    public List<Tarea> getTasks(String username) {
+        String regex = ".*\\|" + username + ";";
+        List<String> tasksRecords = FileManagement.searchInFile(FILE_NAME, regex);
+        List<Tarea> tasks = new ArrayList<Tarea>();
+        for (String taskRecord : tasksRecords) {
+            String[] taskData = taskRecord.split("\\|");
+            Integer id = Integer.parseInt(taskData[0]);
+            String nombre = taskData[1];
+            Integer tiempoEnfoque = Integer.parseInt(taskData[2]);
+            Integer numeroPomodoros = Integer.parseInt(taskData[3]);
+            Tarea tarea = new Tarea(id, nombre, tiempoEnfoque, numeroPomodoros, username);
+            tasks.add(tarea);
+        }
+
+        return tasks;
+    }
+
+    public void deleteTask(Integer id) {
+        String regex = id + "\\|.*;";
+        FileManagement.deleteInFile(FILE_NAME, regex);
+    }
+
+    public void updateTask(Tarea tarea) {
+        String regex = tarea.getId() + "\\|.*;";
+        String content = tarea.getId() + "|" + tarea.getNombre() + "|" + tarea.getTiempoEnfoque() + "|"
+                + tarea.getNumeroPomodoros() + "|" + tarea.getUsername() + ";";
+        FileManagement.updateInFile(FILE_NAME, regex, content);
+    }
 }
