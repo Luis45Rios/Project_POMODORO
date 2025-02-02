0535ffa2abd2f19cd898f2091e8702645369a5e0
Joseph Aguilar <josephadrianaguilar@gmail.com>
2025-02-02 13:28:33 -0500

    Trabaje en usuarioFileManagment y UsuarioController

--- src/Controlador/UsuarioController.java
+++ src/Controlador/UsuarioController.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package Controlador;
-
+import Servicios.UsuarioFileManagment;
 /**
  *
  * @author luisr
  */
 public class UsuarioController {
-    
+    private final UsuarioFileManagment usuarioService = new UsuarioFileManagment();
+
+    public Boolean usuarioExiste(String username) {
+        return usuarioService.userExists(username);
+    }
+
+    public void crearUsuario(String username, String password) {
+        Usuario usuario = new Usuario(username, password);
+        usuarioService.createUser(usuario);
+    }
+
+    public Boolean validarUsuario(String username, String password) {
+        return usuarioService.validateUser(username, password);
+    }
 }
--- src/Servicios/UsuarioFileManagment.java
+++ src/Servicios/UsuarioFileManagment.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package Servicios;
-
 /**
  *
  * @author luisr
  */
 public class UsuarioFileManagment {
-    
+    private static final String FILE_NAME = "users.txt";
+
+    public Boolean userExists(String username) {
+        String regex = username + "\\|.*";
+        return FileManagment.searchOneInFile(FILE_NAME, regex) != null;
+    }
+
+    public void createUser(Usuario usuario) {
+        FileManagment.createFile(FILE_NAME);
+        if (!userExists(usuario.getUsername())) {
+            String content = usuario.getUsername() + "|" + usuario.getPassword() + ";";
+            FileManagment.writeFile(FILE_NAME, content);
+        }
+    }
+
+    public Boolean validateUser(String username, String password) {
+        String regex = username + "\\|" + password + ";";
+        return FileManagment.searchOneInFile(FILE_NAME, regex) != null;
+    }
+
 }
