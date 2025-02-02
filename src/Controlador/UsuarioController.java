a1371ad43cbcc98de99ea01b93d899de1a58a273
Luis <luis.h.rios@unl.edu.ec>
2025-02-02 13:59:34 -0500

    Cambios en Controlador y Servicios

--- src/Controlador/UsuarioController.java
+++ src/Controlador/UsuarioController.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Controlador;
+import Modelos.Usuario;
 import Servicios.UsuarioFileManagment;
 /**
  *
--- src/Servicios/UsuarioFileManagment.java
+++ src/Servicios/UsuarioFileManagment.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package Servicios;
+
+import Modelos.Usuario;
+
 /**
  *
  * @author luisr
